0f5c89390ee3b860b6711dd46ef9e4dd956fdf08
Allan MacLean <allanmaclean@Allans-MacBook-Pro-2.local>
2025-09-17 16:57:02 -0400

    add grid iteration approach. add basic styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  readonly gridSize = 5;
+  readonly rows: number[] = Array.from({ length: this.gridSize }, (_, i) => i).reverse();
+  readonly cols: number[] = Array.from({ length: this.gridSize }, (_, i) => i);
+
   protected readonly title = signal('trs-frontend');
+
   private dataService = inject(DataService);
 
   ngOnInit() {
